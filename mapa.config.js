68f41b25de1a5dbb86692fb511d705bd16221fb5
circleride <circleride.team@gmail.com>
2015-07-21 00:59:12 -0500

    1

--- mapa.config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  firebase: 'burning-inferno-2921',
-  mapbox: {
-      accessToken: 'pk.eyJ1IjoibWFpdHJldCIsImEiOiI1NTMwMTM4YWUxNjYzZWJiMWRlOGU5OTM2MzgzYTliZiJ9.2XQtvaTJpmvXzDvQTJIHNg',
-      mapId: 'maitret.hfmc0blm'
-  }
-};
